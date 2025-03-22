bb57e1bde3797aa77c702d3abac60dd4f9992174
hase <62376587+patrickahase@users.noreply.github.com>
2025-03-23 09:06:51 +1100

    tone exercise update - setting up synth

--- dms3/colourPickerFeature/script.js
+++ dms3/colourPickerFeature/script.js
@@ -11,13 +11,16 @@ function setSquareColour(newColour){
     
     /* this is some built in error checking */
     /* we create a new variable and set it to 'black' as we know that its a supported colour value */
-    let validColour = 'black';
+    let validColour = 'hsl(100, 100%, 50%)';
+
+   
 
     /* this then check if the newColour is a valid colour value */
     /* if it is it changes validColour from 'black' to newColour */
     /* if it isn't it doesn't do anything - so validColour remains 'black' */
     if(CSS.supports('color', newColour)){
         validColour = newColour;
+        console.log(validColour);
     }
 
     /* finally we set the backgroundColor style of colourSquare to the validColour */
@@ -27,3 +30,44 @@ function setSquareColour(newColour){
 
 /* below is an example call to the function : because its just in the script it'll run when the page is loaded */
 setSquareColour('red');
+
+let colourPicker = document.getElementById("colour-picker");
+
+colourPicker.addEventListener("input", pickingColour);
+
+function pickingColour(e){
+  setSquareColour(e.target.value);
+}
+
+function numberToHSL(num){
+  return `hsl(${num}, 100%, 50%)`;
+  let nexMix = `color-mix(in hsl, red ${num}%, blue ${100-num})`;
+}
+
+setSquareColour(numberToHSL(100));
+
+
+
+let para = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+
+let datePicker = document.getElementById("date-picker");
+
+datePicker.addEventListener("change", (e) => {
+  let chosenDate = new Date(e.target.value);
+  let UTC = Date.UTC(
+    chosenDate.getFullYear(),
+    chosenDate.getMonth(),
+    chosenDate.getDate()
+  );
+  let newCol = numberToHSL(UTC%360);
+  setSquareColour(newCol);
+  console.log(newCol);
+});
+
+let foundElementUnique = document.getElementsByClassName("unique");
+let foundElementMany = document.getElementsByClassName("many");
+
+console.log(foundElementUnique);
+console.log(foundElementMany);
+console.log(colourSquare);
--- dms3/informationDesignExercise/script.js
+++ dms3/informationDesignExercise/script.js
@@ -7,36 +7,81 @@ document.getElementById("dialogCloseBtn").addEventListener("click", () => {
 /* to get the backdrop working we need to trigger modal open with js */
 document.getElementById("introDialog").showModal();
 
+/* we set up the map before anything else - this way our variables can be part of the init */
+
+///////// Map Inputs
+/* I would not normally name these variables like this : just for the exercise */
+
+/* this input has a range of 1 - 10 with a 0.1 step : init with value of 1 */
+let variable1RangeInput = document.getElementById("var1Range");
+let variable1 = variable1RangeInput.value;
+
+let oscCount = 3 + (variable1 / 4);
+let oscSpread = 30 + variable1;
+
+document.getElementById("synthRange").addEventListener("input", e => {
+  polySynth.set({oscillator: {spread: 30 * e.target.value }});
+});
+
+///////// Synthesizer
+
+let polySynth = new Tone.PolySynth(Tone.Synth, {
+  oscillator: {
+    type: "fatsawtooth",
+    count: oscCount,
+    spread: oscSpread,
+  },
+  envelope: {
+    attack: 0.01,
+    decay: 0.1,
+    sustain: 0.5,
+    release: 0.1,
+    attackCurve: "exponential",
+  }
+});
+
+///////// Effects
+
+
+///////// Note Sequence
+
+let sequence = [
+  {note: "D3", velocity: 0.9, time: 0},
+  {note: "F3", velocity: 0.9, time: 0},
+  {note: "A4", velocity: 0.9, time: 0},
+  {note: "E4", velocity: 0.9, time: 0},
+
+  {note: "E3", velocity: 0.9, time: "2n"},
+  {note: "G#3", velocity: 0.9, time: "2n"},
+  {note: "B4", velocity: 0.9, time: "2n"},
+  {note: "D4", velocity: 0.9, time: "2n"},
+];
+
+let part = new Tone.Part(((time, value) => {
+  polySynth.triggerAttackRelease(value.note, "2n", time, value.velocity);
+}), sequence).start(0);
+
+part.loop = true;
+part.humanize = true;
+part.probability = 0.8;
+
+///////// Init Connections
+
 function toneInit(){
-  /* grainPlayer.toDestination();
-  grainPlayer.start(); */
-  grainPlayer = new Tone.GrainPlayer({
-    url: buffer,
-    loop: true,
-    grainSize: 0.01,
-    overlap: 0.04,
-    playbackRate: 0.01,
-    reverse: false
-  }).toDestination();
+
+  polySynth.toDestination();
+
+  
 }
 
 
-/////////////// Setting up the GrainPlayer
 
-/* set up audio buffer : easy access storage for audio file */
 
-const buffer = new Tone.ToneAudioBuffer("./media/guessSo.mp3");
 
-let grainPlayer;
 
 document.getElementById("playbackToggle").addEventListener("click", () => {
-  grainPlayer.start(Tone.now());
+  Tone.Transport.start();
 });
-
-
-
-
-
 
 
 
